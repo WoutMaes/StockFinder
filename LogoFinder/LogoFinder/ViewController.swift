3a90f6e938109c9297feb4fd80eb17cb96431eab
Wout Maes <wout.maes@student.hogent.be>
2019-09-25 09:59:13 +0200

    first commit

---
+++ LogoFinder/LogoFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogoFinder
+//
+//  Created by woutmaes on 25/09/2019.
+//  Copyright © 2019 woutmaes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
